0c818b7921009dc84a8625a990704ec488936303
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-12-26 07:38:58 +0100

    01 - Variables & Basic Types

--- demo/variables/variables.ts
+++ demo/variables/variables.ts
@@ -9,3 +9,43 @@
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/variable-declarations.html#let-declarations
 //
+
+const courseName = 'typescript';
+const courseName2 = 'typescript';
+const courseName3 = `typescript`;
+
+const amount = 10;
+const fraction = 10.5;
+const oneThousand = 1e3; // 1000 - exponents
+
+const allPermissions = 0o777; // octal
+const hexByte = 0xff; // hexadecimal
+const binary = 0b0100001; // binary
+
+const bigInt = 9000n; // big integer
+
+const yes = true;
+const no = false;
+
+const missing = undefined; // completely missing
+
+const empty = null; // we know it's missing
+
+let someNum = 0;
+someNum = 1;
+someNum = 2;
+
+// let someNum = 5; // error - cannot redeclare block-scoped variable
+
+{
+  let someNum = 5; // shadowing variable - variable's scope is limited to the block and value will be 5
+}
+
+// here someNum is 2
+
+let hello; // here hello is undefined
+hello = 'hi';
+hello = 'hey';
+hello = 'hello';
+
+// const whoops; // error - 'const' declarations must be initialized
