68564a2fddbb4fb09562dbd8eb56c5a82c552774
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-12-29 07:55:59 +0100

    03 - Compound Boolean Expressions

--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,4 +1,27 @@
 /* eslint-disable */
-import { strict as assert } from "assert";
+import { strict as assert } from 'assert';
 
+// NOT
+const writing = true;
+const reading = !writing; // false
+assert.equal(reading, false);
 
+// OR
+// (pick 1 movie)
+const rating = 9;
+const favoriteMovie = false;
+const suggestMovie = rating > 8 || favoriteMovie; // true
+assert.equal(suggestMovie, true);
+
+// AND
+const age = 18;
+const isTeenager = age >= 13 && age < 20; // true
+assert.equal(isTeenager, true);
+
+// our package
+const packageWeight = 30;
+const packageLength = 50;
+const feeExemption = false;
+
+const extraFee = !feeExemption && (packageWeight > 25 || packageLength > 40); // true
+assert.equal(extraFee, true);
