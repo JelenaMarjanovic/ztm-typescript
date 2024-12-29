091f5db00654ae3c935b9b9cd8e2561546d40041
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-12-29 08:46:38 +0100

    05 - Increment & Arithmetic Assignment

--- demo/increment/increment.ts
+++ demo/increment/increment.ts
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import { strict as assert } from "assert";
+import { strict as assert } from 'assert';
 
 // Incrementing numbers is a common task to perform when writing programs. So
 // common that there is an operator dedicated to just incrementing numbers.
@@ -8,3 +8,49 @@ import { strict as assert } from "assert";
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
 //
+
+let n = 1;
+
+// "postfix" increment
+n++;
+assert.equal(n, 2);
+
+// "prefix" increment
+++n;
+assert.equal(n, 3);
+
+// DIFFERENCE
+n = 5;
+const k = n++; // k = 5, n = 6
+assert.equal(n, 6);
+assert.equal(k, 5);
+
+n = 5;
+const j = ++n; // j = 6, n = 6
+assert.equal(n, 6);
+assert.equal(j, 6);
+
+// "postfix" decrement
+n = 5;
+const t = n--; // t = 5, n = 4
+assert.equal(n, 4);
+assert.equal(t, 5);
+
+// "prefix" decrement
+n = 5;
+const s = --n; // s = 4, n = 4
+assert.equal(n, 4);
+assert.equal(s, 4);
+
+// Arithmetic assignment
+n = 5;
+n += 5; // n = 10
+assert.equal(n, 10);
+n -= 5; // n = 5
+assert.equal(n, 5);
+n *= 5; // n = 25
+assert.equal(n, 25);
+n /= 5; // n = 5
+assert.equal(n, 5);
+n %= 5; // n = 0
+assert.equal(n, 0);
